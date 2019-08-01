4cd6513c34fe10d06e2e2ed485959c3eda25a8a2
Aemonjabbar <49372579+Aemonjabbar@users.noreply.github.com>
2019-08-01 21:49:11 +0500

    Add files via upload

---
+++ Lab 9/Task3.java
@@ -0,0 +1,48 @@
+class Task3
+{ 
+    String str = "a";
+
+     void A() 
+     { 
+     try
+    { 
+        str +="b"; 
+        B(); 
+    } 
+    catch (Exception e) 
+    { 
+         str += "c"; 
+    } 
+         
+    } 
+    void B() throws Exception 
+    { 
+    try
+    { 
+        str += "d";
+        C();
+        } 
+        catch(Exception e) 
+        { 
+          throw new Exception();
+        }
+        finally 
+        {
+            str += "e";
+        } 
+            str += "f";
+        } 
+        void C() throws Exception
+        {
+            throw new Exception();
+        }
+            void display()
+            {
+                System.out.println(str);
+            }
+ public static void main(String[] args)
+ {
+     Task3 object = new Task3();
+      object.A(); object.display();
+    }
+}
